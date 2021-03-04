0547851ce9d140725afe94c38fece82cbd749289
asif2211 <asifbsit@gmail.com>
2021-03-04 11:34:48 +0500

    feat(controlled Component) add input fileds from helpers ,

--- src/Pages/Customer/Customer.js
+++ src/Pages/Customer/Customer.js
@@ -12,45 +12,38 @@ import {
 import { InputData } from "../../utils/helper";
 import ButtonText from "../../components/ButtonText/ButtonText";
 const Customer = () => {
-  const [inputFields, setInputFields] = useState([
-    { firstName:'' },
-  ]);
-  const handleChangeInput = (index, event) => {
-    alert(index)
-    event.preventDefault();
-    const newInputFields = [...inputFields];
-    newInputFields[index][event.target.name] = event.target.values;
-    setInputFields(newInputFields);
+  const [inputList, setInputList] = useState();
+  const hanleChange  = (e)=>{
+    setInputList({
+      [e.target.name]:e.target.value
+    })
   }
 
-  console.log(inputFields)
   return (
     <Wrapper>
       <Title>Customer Information</Title>
-      
-        {inputFields.map((inputFiled,index) => (
-          <InputContainer key={Math.random()}>
-            <Label> :</Label>
-            <Input
-              name="name"
-              value={inputFiled.firstName}
-              onChange={(event) => handleChangeInput(index,event)}
-            />
-           
-          </InputContainer>
-          
-        ))}
-
-        <Button>
-          <ButtonText
-            // disabled={this.state.fname === "" || this.state.lname === ""}
-            children="Submit"
-            // onClick={this.handleSubmit}
-          >
-            Submit
-          </ButtonText>
-        </Button>
      
+      {InputData.map((x, i) => (
+        <InputContainer key={Math.random()}>
+         
+          <Label> {x.label  }:</Label>
+         <Input
+         name={x.name}
+         value={inputList}
+         onChange={hanleChange}/>
+         
+        </InputContainer>)
+      )}
+
+      <Button>
+        <ButtonText
+          // disabled={this.state.fname === "" || this.state.lname === ""}
+          children="Submit"
+          // onClick={this.handleSubmit}
+        >
+          Submit
+        </ButtonText>
+      </Button>
     </Wrapper>
   );
 };
